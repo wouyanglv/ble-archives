70aaf04829da4ca041e2902f6729337f72fa5082
wouyanglv <63123219+wouyanglv@users.noreply.github.com>
2021-11-22 14:38:25 -0600

    Delete nRF5_SDK_17.0.0_9d13099/examples/ble_peripheral/eeg_emg_v1_samplespacked_PCB_app_BLEtemperature directory

--- nRF5_SDK_17.0.0_9d13099/examples/ble_peripheral/eeg_emg_v1_samplespacked_PCB_app_BLEtemperature/pca10040/s132/ses/Output/Debug/Obj/eeg_emg_pca10040_s132/utf.d
+++
@@ -1,6 +0,0 @@
-Output/Debug/Obj/eeg_emg_pca10040_s132/utf.o: \
- C\:\Users\Rogers\ Group\Dropbox\ (MIT)\Wei\ Ouyang\ -\ BLE\nRF5_SDK_17.0.0_9d13099\external\utf_converter\utf.c \
- C\:\Users\Rogers\ Group\Dropbox\ (MIT)\Wei\ Ouyang\ -\ BLE\nRF5_SDK_17.0.0_9d13099\external\utf_converter\utf.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stddef.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/__crossworks.h \
- C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h
