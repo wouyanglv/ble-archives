892f19b74a0afaa0242c2c9d5a4a97a817cd9f76
wouyanglv <63123219+wouyanglv@users.noreply.github.com>
2021-11-22 14:44:07 -0600

    Delete nRF5_SDK_17.0.0_9d13099/examples/ble_peripheral/Implantable MA - iis3dwb directory

--- nRF5_SDK_17.0.0_9d13099/examples/ble_peripheral/Implantable MA - iis3dwb/pca10040/s132/ses/Output/eeg_emg_pca10040_s132 Release/Obj/iis3dwb_reg.d
+++
@@ -1,5 +0,0 @@
-Output/eeg_emg_pca10040_s132\ Release/Obj/iis3dwb_reg.o: \
- C:\Users\64408\Desktop\nRF5_SDK_16.0.0_98a08e2\ old\examples\ble_peripheral\Implantable\ MA\ -\ Original\iis3dwb_reg.c \
- C:\Users\64408\Desktop\nRF5_SDK_16.0.0_98a08e2\ old\examples\ble_peripheral\Implantable\ MA\ -\ Original\iis3dwb_reg.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52/include/math.h
